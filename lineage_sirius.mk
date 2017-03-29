7e063285b8819e7a15b98fb0f77c9e6d832a2059
Luca Stefani <luca020400@lineageos.org>
2017-03-29 13:49:50 +0200

    sirius: Define product properties according to stock
    Change-Id: I69ce166a712c406211991ae1a8e3ccf6c7809d28

--- lineage_sirius.mk
+++ lineage_sirius.mk
@@ -5,6 +5,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+PRODUCT_DEVICE := sirius
+PRODUCT_NAME := lineage_sirius
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 8 SE
+PRODUCT_MANUFACTURER := xiaomi
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sirius-user 10 QKQ1.190828.002 V12.5.1.0.QEBCNXM release-keys"
 
