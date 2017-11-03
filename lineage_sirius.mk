f555a313c426fdc43503257906b5aab6039fd7cc
Dan Pasanen <dan.pasanen@gmail.com>
2017-11-02 22:04:32 -0500

    sirius: Inherit core_64_bit.mk

--- lineage_sirius.mk
+++ lineage_sirius.mk
@@ -1,3 +1,6 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sirius-user 10 QKQ1.190828.002 V12.5.1.0.QEBCNXM release-keys"
 
