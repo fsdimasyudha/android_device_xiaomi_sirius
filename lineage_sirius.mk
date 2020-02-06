1c18f2c76545b43d952fb75dc49ecad08dddf9f6
Isaac Chen <tingyi364@gmail.com>
2020-02-06 06:29:59 +0100

    sirius: Define build fingerprint
    Signed-off-by: Isaac Chen <tingyi364@gmail.com>

---
+++ lineage_sirius.mk
@@ -0,0 +1,4 @@
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sirius-user 10 QKQ1.190828.002 V12.5.1.0.QEBCNXM release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/sirius/sirius:10/QKQ1.190828.002/V12.5.1.0.QEBCNXM:user/release-keys
