7d142a3d672fff615d9449c6b888130b6577a3fc
Mesquita <mesquita@aospa.co>
2022-09-18 13:02:55 -0300

    sirius: build android.hardware.bluetooth.audio@2.0-impl
     * Required for calls over bluetooth devices
    
    Signed-off-by: Mesquita <mesquita@aospa.co>
    Change-Id: I4ecdc36cf4178f68a37c06e0fd48c6306b7fd153

--- sirius.mk
+++ sirius.mk
@@ -132,6 +132,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
     BluetoothQti \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
