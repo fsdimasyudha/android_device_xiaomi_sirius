bba0f3dbccee85f1f10b7323e7c3264ed3c34e7a
SathamHussainM <sathamhussain.m11@gmail.com>
2022-09-23 06:54:06 +0000

    sirius: Add common_full_phone.mk
    Signed-off-by: SathamHussainM <sathamhussain.m11@gmail.com>

--- lineage_sirius.mk
+++ lineage_sirius.mk
@@ -2,6 +2,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sirius-user 10 QKQ1.190828.002 V12.5.1.0.QEBCNXM release-keys"
 
