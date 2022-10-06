7c73550d8ca598478adf0c3023c759088b139a11
Dimas Yudha Pratama <fsdimasyudha@gmail.com>
2022-10-06 11:37:40 +0700

    sirius: Import props from stock MIUI ROM
     * Based on MIUI 12.5 V12.5.1.0.QEBCNXM

---
+++ BoardConfig.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
+TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
