81cb2f6c5b37325833e8b94554d17c3e94c959f2
Dimas Yudha Pratama <fsdimasyudha@gmail.com>
2022-10-07 18:23:49 +0700

    sirius: Import HIDL from stock MIUI ROM
     * Based on MIUI 12.5 V12.5.1.0.QEBCNXM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
